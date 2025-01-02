a3c09e5f7a4939e5ef852301031abb844d42e846
mor7495@gmail.com <mor7495@gmail.com>
2025-01-02 18:49:57 +0700

    assets/img/Thailand/... Fix

--- src/assets/img/Thailand/KohLanta/index.js
+++ src/assets/img/Thailand/KohLanta/index.js
@@ -1,15 +1,17 @@
-import attr3 from "./attr3.jpg";
-import rest4 from "./rest4.jpg";
-
-import night1 from "./night1.jpg";
-import night2 from "./night2.jpg";
-import night3 from "./night3.jpg";
-
 import main from "./main.jpg";
 
 import { importAll } from "../../../../helpers/imagesHelpers";
 export const beachTravelImages = importAll(
   require.context("./beach-travel", false, /\.(png|jpe?g|svg)$/),
+);
+export const nightLife = importAll(
+  require.context("./nightLife", false, /\.(png|jpe?g|svg)$/),
+);
+export const attractionsMarket = importAll(
+  require.context("./attractionsMarket", false, /\.(png|jpe?g|svg)$/),
+);
+export const restaurantsMoonwalk = importAll(
+  require.context("./restaurantsMoonwalk", false, /\.(png|jpe?g|svg)$/),
 );
 
 export const shellsImages = importAll(
@@ -32,8 +34,8 @@ export const KHO_LANTA_IMAGES = {
   hotelLantaRiviera: LantaRivieraImages,
   beachTravelImages: beachTravelImages,
   attractionsFollowingGiants: FollowingGiantsImages,
-  attractionsMarket: [attr3],
+  attractionsMarket: attractionsMarket,
   restaurantsLantaRiviera: RestaurantLantaRivieraImages,
-  restaurantsMoonwalk: [rest4],
-  nightLife: [night1, night2, night3],
+  restaurantsMoonwalk: restaurantsMoonwalk,
+  nightLife: nightLife,
 };
