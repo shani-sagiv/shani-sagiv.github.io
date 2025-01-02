45eaec8e501032d9e580408e4720caeb791d1002
mor7495@gmail.com <mor7495@gmail.com>
2025-01-02 18:58:50 +0700

    assets/img/Vietnam

--- scripts/images.js
+++ scripts/images.js
@@ -2,7 +2,7 @@ const fs = require("fs-extra");
 const path = require("path");
 const sharp = require("sharp");
 
-const assetsDir = path.join(__dirname, "../src", "assets/img/Thailand"); // עבודה בתיקיית src/assets
+const assetsDir = path.join(__dirname, "../src", "assets/img/Vietnam/hanoi"); // עבודה בתיקיית src/assets
 const supportedFormats = [".jpg", ".jpeg", ".png"]; // פורמטים נתמכים בלבד
 const suffix = "_compressed"; // הסיומת שמתווספת לקובץ שכבר כווץ
 const MIN_FILE_SIZE = 300 * 1024; // גודל מינימלי ב-Bytes (300KB)
--- src/assets/img/Thailand/KohPhaNgan/index.js
+++ src/assets/img/Thailand/KohPhaNgan/index.js
@@ -1,6 +1,6 @@
 import { importAll } from "../../../../helpers/imagesHelpers";
-import sunset from "./sunset.jpeg";
-import mamaRestaurant from "./mama.jpeg";
+import sunset from "./sunset_compressed.jpeg";
+import mamaRestaurant from "./mama_compressed.jpeg";
 export const islandLifeImages = importAll(
   require.context("./island-life", false, /\.(png|jpe?g|svg)$/),
 );
