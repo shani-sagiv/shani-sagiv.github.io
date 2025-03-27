b6a25f289471e83ec75444ec48b75f0165d719eb
mor7495@gmail.com <mor7495@gmail.com>
2025-03-27 15:45:16 +0700

    wipp

--- src/components/Recommendation/Recommendation.tsx
+++ src/components/Recommendation/Recommendation.tsx
@@ -59,8 +59,7 @@ const Recommendation: React.FC<RecommendationProps> = ({ recommendation }) => {
           display: "flex",
           flexDirection: "column",
           alignItems: "center",
-          width: "min(97vw, 370px)",
-          maxWidth: "92vw",
+          width: "min(97vw, 400px)",
 
           gap: 5,
           margin: "5px 1.5vw 10px 1.5vw",
